be77f87d80e4b9b1f7e27e91566ce2a7a802db9e
Edoardo Lolletti <edoardo762@gmail.com>
2026-06-30 23:48:30 +0200

    Update ez5h.s

--- blocksds/source/platform/ez5h/ez5h.s
+++ blocksds/source/platform/ez5h/ez5h.s
@@ -470,7 +470,8 @@ BEGIN_ASM_FUNC ez5h_writeMultipleSector
 BEGIN_ASM_FUNC_NO_SECTION ez5h_readMultipleSector
 	ldr	r3, ez5h_readSector_addr
 save_regs_and_switch_to_thumb:
-	push {r4-r12,lr}
+	@ push r0,r2,r3 so that they can be popped in the right regs below
+	push {r0,r2,r3,r4-r12,lr}
 	adr r4, sdio_functions
 	ldmia r4!, {r9,r10,r11,r12}
 	orr r4, #1
@@ -499,10 +500,9 @@ ez5h_writeSector_sdio4BitCrc16:
 @ bool doOperation(uint32_t sector, uint32_t num_sectors, void* buffer, bool(*operation)(u32 sector, void* buffer))
 @ BEGIN_ASM_FUNC_NO_SECTION doSDOperation thumb
 doSDOperation:
-	push {r3-r7, lr}
-	movs r4, r0
-	movs r5, r2
-	movs r7, r3
+	@ these are the og r0,r2,r3 that got pushed in the entrypoint
+	pop {r4,r5,r7}
+	push {r3-r6,lr}
 	adds r6, r0, r1
 
 check_next_sector:
